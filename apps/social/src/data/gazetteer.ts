9be72e1a92fa93aeeca6593715dfb7724962658f
peng <shileipeng@gmail.com>
2026-06-02 15:05:15 +0800

    Fix gazetteer JSON import issue by converting to TypeScript module

---
+++ apps/social/src/data/gazetteer.ts
@@ -0,0 +1,58 @@
+export const gazetteerData = {
+  countries: [
+    {
+      code: "US",
+      regions: [
+        { code: "CA", cities: ["San Francisco", "Los Angeles", "San Diego", "San Jose"] },
+        { code: "MA", cities: ["Boston", "Cambridge"] },
+        { code: "NY", cities: ["New York", "Buffalo"] },
+        { code: "WA", cities: ["Seattle", "Bellevue"] },
+      ],
+    },
+    {
+      code: "CN",
+      regions: [
+        { code: "BJ", cities: ["Beijing"] },
+        { code: "SH", cities: ["Shanghai"] },
+        { code: "GD", cities: ["Guangzhou", "Shenzhen"] },
+        { code: "ZJ", cities: ["Hangzhou", "Ningbo"] },
+      ],
+    },
+    {
+      code: "GB",
+      regions: [
+        { code: "ENG", cities: ["London", "Manchester", "Birmingham"] },
+        { code: "SCT", cities: ["Edinburgh", "Glasgow"] },
+      ],
+    },
+    {
+      code: "DE",
+      regions: [
+        { code: "BE", cities: ["Berlin"] },
+        { code: "BY", cities: ["Munich"] },
+        { code: "HH", cities: ["Hamburg"] },
+      ],
+    },
+    {
+      code: "FR",
+      regions: [
+        { code: "IDF", cities: ["Paris"] },
+        { code: "ARA", cities: ["Lyon"] },
+      ],
+    },
+    {
+      code: "JP",
+      regions: [
+        { code: "13", cities: ["Tokyo"] },
+        { code: "27", cities: ["Osaka"] },
+      ],
+    },
+    {
+      code: "KR",
+      regions: [
+        { code: "11", cities: ["Seoul"] },
+        { code: "26", cities: ["Busan"] },
+      ],
+    },
+  ],
+};
